ca67c2182846feb4cc5730906535f44333ec7420
Hassan Khalil <hfk08@mail.aub.edu>
2024-12-12 11:22:07 +0200

    Delete api directory

--- api/services/get_doctor_service.py
+++
@@ -1,6 +0,0 @@
-from api.models.hospital import Doctor
-
-
-def get_doctor(disease):
-    doctor = Doctor.objects.filter(specialty__disease=disease).first()
-    return doctor
--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from api.views.hospital_view import PredictDoctorView
-from django.urls import path
-
-urlpatterns = [
-    path("recommend-doctor/", view=PredictDoctorView.as_view(), name="recommend-doctor"),
-]
